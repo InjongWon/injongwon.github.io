f78217927af77b4c734db03da5daf7d4762094e4
Injong Won <injong.won@ibm.com>
2020-02-26 13:47:54 -0500

    added scripts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { AppComponent } from './app.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '404', component: NotfoundComponent },
+  { path: '**', redirectTo: '/404'}
+];
 
 @NgModule({
+  // declarations:[
+  //   AppComponent,
+  //   NotfoundComponent
+  // ],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { PaginationComponent } from './pagination/pagination.component';
 
+
+
+// const routes: Routes = [
+
+// ]
 @NgModule({
   declarations: [
-    AppComponent
+    // AppRoutingModule //Remove this module and put it into import
+    AppComponent,
+    NotfoundComponent,
+    PaginationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule // imported here
   ],
   providers: [],
   bootstrap: [AppComponent]
